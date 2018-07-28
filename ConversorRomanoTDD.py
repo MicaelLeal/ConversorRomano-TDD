b9e893ca8e97930f8cb6f828bb5fc14818825c6d
MicaelLeal <mycaellmoura@hotmail.com>
2018-07-28 11:46:33 -0300

    Avançozinho de leves nos testes

--- ConversorRomanoTDD.py
+++ ConversorRomanoTDD.py
@@ -3,62 +3,69 @@ class ConversorRomano(object):
 		super(ConversorRomano, self).__init__()
 		self.algarismos = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
 
-	def converterAlgarismo(self, letra):
-		for algarismo in self.algarismos:
-			if (algarismo == letra):
-				return self.algarismos[algarismo]
+	def converterRecursivo(self, numeroRomano):
+		tam = len(numeroRomano)
+		if (tam == 1) :
+			for algarismo in self.algarismos:
+				if (algarismo == numeroRomano):
+					numeroArabico = self.algarismos[algarismo]
+		else:
+			numeroArabico = self.converterRecursivo(numeroRomano[0])
+			for i in range(1, tam):
+				valorDoAtual = self.converterRecursivo(numeroRomano[i])
+				valorDoAnterior = self.converterRecursivo(numeroRomano[i-1])
+				if (valorDoAtual <= valorDoAnterior):
+					numeroArabico += valorDoAtual
+				else:
+					numeroArabico -= valorDoAnterior
+					numeroArabico += valorDoAtual - valorDoAnterior
+
+		return numeroArabico
 
 	def converter(self, numeroRomano):
 		numeroRomano = numeroRomano.upper()
 		self.validarNumeroRecebido(numeroRomano)
-
-		numeroArabico = self.converterAlgarismo(numeroRomano[0])
-		tam = len(numeroRomano)
-		if (tam > 1):
-			for i in range(1, tam):
-				valorDoAtual = self.converterAlgarismo(numeroRomano[i])
-				valorDoAnterior = self.converterAlgarismo(numeroRomano[i-1])
-				if (valorDoAtual <= valorDoAnterior):
-					if (valorDoAtual > numeroArabico):
-						raise ValueError("Numero Romano passado eh invalido: "+ numeroRomano)
-					numeroArabico += valorDoAtual
-				else:
-					numeroArabico -= valorDoAnterior
-					if (numeroArabico < 0):
-						raise ValueError("Numero Romano passado eh invalido: "+ numeroRomano)
-					numeroArabico += valorDoAtual - valorDoAnterior
-
-			if (numeroArabico in self.algarismos.values()):
-				raise ValueError("Numero Romano passado eh invalido: "+ numeroRomano)
-
-		return numeroArabico
+		return self.converterRecursivo(numeroRomano)
 
 		
 
 	def validarNumeroRecebido(self, numeroRomano):
 		tam = len(numeroRomano)
+		mensagem = ""
 		if (tam <= 1):
 			if numeroRomano not in self.algarismos:
-				raise ValueError("Algarismo Romano passado eh invalido: "+ numeroRomano)
+				mensagem = "Algarismo passado eh invalido: "+ numeroRomano
 		else:
 			for letra in numeroRomano:
 				if letra not in self.algarismos:
-					raise ValueError("Numero Romano passado contem algarismos invalidos: "+ numeroRomano)
+					mensagem = "Numero Romano passado contem algarismos invalidos: "+ numeroRomano
 
-			algarismosInvalidos = ["VV", "LL", "DD", "IIII", "XXXX", "CCCC", "MMMM"]
+			if self.converterRecursivo(numeroRomano) in self.algarismos.values():
+				mensagem = "Numero Romano passado possui sequencia de algarismos invalida: "+ numeroRomano
+
+			algarismosInvalidos = ["IIII", "XXXX", "CCCC", "MMMM"]
 			for algarismo in algarismosInvalidos:
 				if algarismo in numeroRomano:
-					raise ValueError("Numero Romano passado possui sequencia de algarismos invalida: "+ numeroRomano)
+					mensagem = "Numero Romano passado possui sequencia de algarismos invalida: "+ numeroRomano
 
 			for i in range(1, tam):
-				valorDoAtual = self.converterAlgarismo(numeroRomano[i])
-				valorDoAnterior = self.converterAlgarismo(numeroRomano[i-1])
-				if valorDoAtual > valorDoAnterior:
-					if (valorDoAnterior == 5 or valorDoAnterior == 50 or valorDoAnterior == 500 or (valorDoAnterior*5 != valorDoAtual and valorDoAnterior*10 != valorDoAtual)):
-						raise ValueError("Numero Romano passado possui sequencia de algarismos invalida: "+ numeroRomano)
+				valorDoAtual = self.converterRecursivo(numeroRomano[i])
+				valorDoAnterior = self.converterRecursivo(numeroRomano[i-1])
+				valorCalculado = self.converterRecursivo(numeroRomano[:i])
+				valoresNaoRepetiveis = [5, 50, 500]
 
+				if (valorDoAtual > valorDoAnterior):
+					if (valorDoAnterior*5 != valorDoAtual and valorDoAnterior*10 != valorDoAtual):
+						mensagem = "asdf2"
 
-		
+				if (valorDoAtual >= valorDoAnterior):
+					if (valorDoAnterior in valoresNaoRepetiveis):
+						mensagem = "Nao repetiveis repetindo"
+					if (valorCalculado < valorDoAtual and valorCalculado not in self.algarismos.values()):
+						mensagem = "deu bom"
+
+		if (mensagem != ""):
+			raise ValueError(mensagem)		
 
 class TestConversorRomano(object):
 
@@ -105,10 +112,16 @@ class TestConversorRomano(object):
 		algarismosInvalidos = ["IVI", "IIX", "CMM", "IXL", "IIV","IIX","XXL","XXC","CCD","CCM","IVI","IXI","IXL","XCM"]
 		for algarismo in algarismosInvalidos:
 			try:
+				print(algarismo)
 				ConversorRomano().converter(algarismo)
 				assert False
 			except ValueError as e:
 				assert True
+
+	def test_deve_converter_numeros_romanos_validos_com_mais_de_dois_algarismos(self):
+		algarismos = {"XIX":19, "XLIV":44, "LXXIII":73, "LXXXVIII":88, "XCIX":99, "CMXXV":925, "MMCCC":2300}
+		for algarismo in algarismos:
+			assert ConversorRomano().converter(algarismo) == algarismos[algarismo]
 
 	def test_deve_rejeitar_numeros_romanos_com_mais_de_tres_algarismos_repetidos(self):
 		algarismosInvalidos = ["IIII", "XXXX", "CCCC", "MMMM"]
@@ -119,12 +132,17 @@ class TestConversorRomano(object):
 			except ValueError as e:
 				assert True
 
-	def test_deve_converter_numeros_romanos_validos_com_mais_de_dois_algarismos(self):
-		algarismos = {"XIX":19, "XLIV":44, "LXXIII":73, "LXXXVIII":88, "XCIX":99, "CMXXV":925, "MMCCC":2300}
-		for algarismo in algarismos:
-			assert ConversorRomano().converter(algarismo) == algarismos[algarismo]
+	def test_deve_rejeitar_numeros_romanos_invalidos_com_mais_de_tres_algarismos(self):
+		algarismosInvalidos = ["MXVVL", "XXVIIV", "MMLVIIIX"]
+		for algarismo in algarismosInvalidos:
+			try:
+				ConversorRomano().converter(algarismo)
+				assert False
+			except ValueError as e:
+				assert True
+
 			
 def main():
-	print(ConversorRomano().converter("MMLVIIIX"))
+	print(ConversorRomano().converter(input()))
 if __name__ == '__main__':
 	main()
